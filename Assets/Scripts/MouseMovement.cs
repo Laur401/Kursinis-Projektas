8c0862154c2cd884e49d1517562913369daa4841
Laur401 <laurynastautvydas11@gmail.com>
2025-03-31 23:28:27 +0300

    Pridėtas kamuoliuko mušimo greičio limiteris, mušimo kampo taisymas, linijos piešimo limiteris.

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder.MeshOperations;
+using UnityEngine.Rendering.PostProcessing;
 
 public class MouseMovement : MonoBehaviour
 {
@@ -29,6 +30,8 @@ public class MouseMovement : MonoBehaviour
         {
             Vector3? worldPoint = CastMouseClickRay();
             if (!worldPoint.HasValue) return;
+            worldPoint = ClampMousePoint(worldPoint.Value);
+            
             DrawLine(worldPoint.Value);
         
             if (Input.GetMouseButtonDown(0))
@@ -78,11 +81,23 @@ public class MouseMovement : MonoBehaviour
         horizontalWorldPoint.y = transform.position.y;
 
         Vector3 direction = (horizontalWorldPoint - transform.position).normalized;
-        float strength = Vector3.Distance(transform.position, horizontalWorldPoint);
+        float strength = Mathf.Clamp(Vector3.Distance(transform.position, horizontalWorldPoint), 0f, 10f);
+
+        direction = AdjustVelocityDirectionToSlope(direction);
         
         rb.AddForce(direction * (strength * shotPower));
         
         lsm.AddStroke();
+    }
+
+    private Vector3 ClampMousePoint(Vector3 worldPoint)
+    {
+        return Vector3.MoveTowards(gameObject.transform.position, worldPoint, 10f);
+        if (Vector3.Distance(gameObject.transform.position, worldPoint) > 30f)
+        {
+            return Vector3.MoveTowards(gameObject.transform.position, worldPoint, 30f);
+        }
+        else return worldPoint;
     }
     
     private void CheckMovement() //TODO: Figure out how to optimize this to not need to be called every frame.
@@ -99,6 +114,19 @@ public class MouseMovement : MonoBehaviour
             lineRenderer.enabled = false;
             isMoving = true;
         }
+    }
+
+    private Vector3 AdjustVelocityDirectionToSlope(Vector3 velocity)
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, 0.2f))
+        {
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            var adjustedVelocity = slopeRotation * velocity;
+            return adjustedVelocity;
+        }
+
+        return velocity;
     }
 
 }
